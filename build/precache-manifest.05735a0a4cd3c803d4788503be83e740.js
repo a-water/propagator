ebdba48e529d4ae3b1dfb1113981f97450f6c3c4
rose-water <rachel.ulgado@gmail.com>
2018-12-22 16:47:49 -0800

    hm

---
+++ build/precache-manifest.05735a0a4cd3c803d4788503be83e740.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "00f39c89db9168aeb9009d21cc190260",
+    "url": "/static/media/secondary.00f39c89.png"
+  },
+  {
+    "revision": "06f516086ccef30df81e",
+    "url": "/static/css/main.5c508f7d.chunk.css"
+  },
+  {
+    "revision": "a89b6c4095ec9352fb37",
+    "url": "/static/js/1.a89b6c40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63c9c617c2fde3e5d17a42a5ff84eb4f",
+    "url": "/static/media/sparkle stairs still.63c9c617.jpg"
+  },
+  {
+    "revision": "8a75912d2c832489884e8a0ecffbe9ca",
+    "url": "/static/media/subunit.8a75912d.jpeg"
+  },
+  {
+    "revision": "d90c5f6592f7cc67ed2e643f50b0b853",
+    "url": "/static/media/windy road.d90c5f65.jpeg"
+  },
+  {
+    "revision": "ed51774eb067620e24abe6bd9446cd28",
+    "url": "/static/media/sleep paralysis.ed51774e.jpeg"
+  },
+  {
+    "revision": "9617a76fbb664ea8538b2fa3de4d05af",
+    "url": "/static/media/primary.9617a76f.png"
+  },
+  {
+    "revision": "fc83ca5b05ed6a95ccd3181ea03d770c",
+    "url": "/static/media/secondary.fc83ca5b.png"
+  },
+  {
+    "revision": "d4b680864378db25ea7948ae681ab733",
+    "url": "/static/media/primary.d4b68086.png"
+  },
+  {
+    "revision": "06f516086ccef30df81e",
+    "url": "/static/js/main.06f51608.chunk.js"
+  },
+  {
+    "revision": "eeba7cf032c3da9fe022b0e0e7bc317a",
+    "url": "/static/media/tertiary.eeba7cf0.png"
+  },
+  {
+    "revision": "a81516e0006d7ceb033e5b1fa34ad481",
+    "url": "/static/media/secondary.a81516e0.png"
+  },
+  {
+    "revision": "4cecd9c1bba827a5870a3715753f8e08",
+    "url": "/static/media/primary.4cecd9c1.png"
+  },
+  {
+    "revision": "720d70aad2ed8e8e461ae6d37efca4c8",
+    "url": "/static/media/secondary.720d70aa.png"
+  },
+  {
+    "revision": "e3d5c9862bffca7bae63fa88c48a0d29",
+    "url": "/static/media/primary.e3d5c986.jpg"
+  },
+  {
+    "revision": "6045978aee94da2e9708f376d07ca303",
+    "url": "/static/media/secondPrimary.6045978a.png"
+  },
+  {
+    "revision": "f4d54a12f4cc03f4c07c51258b84e77d",
+    "url": "/static/media/secondPrimary.f4d54a12.jpg"
+  },
+  {
+    "revision": "470a7ffbad317973b71122104c8d0959",
+    "url": "/static/media/secondary.470a7ffb.png"
+  },
+  {
+    "revision": "781a2d54b4448f156063b28e868e9560",
+    "url": "/static/media/tertiary.781a2d54.png"
+  },
+  {
+    "revision": "94e980864b7a15985b8aa1e04d41d382",
+    "url": "/index.html"
+  }
+];
