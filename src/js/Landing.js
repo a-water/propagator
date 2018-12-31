0ed1b25a237bdfca712d413f59b3016072a0131e
rose-water <rachel.ulgado@gmail.com>
2018-12-30 20:28:00 -0800

    col

--- src/js/Landing.js
+++ src/js/Landing.js
@@ -15,18 +15,16 @@ class Landing extends Component {
           <div id="landing-about">
             <h1 id="landing-title">PROPAGATOR</h1>
             <p id="landing-description">is a collaborative zine, crafted by a community of peers working across creative disciplines such as art, design, and technology.
-            <br/> <br/>Its inception came out of a need for time and space - for experimenting and engaging with different approaches to creating art, for expanding our horizons and creative practices, and most importantly, to develop a better understanding of ourselves and our place in the world.</p>
+            <br/><br/>Its inception came out of a need for time and space – for experimenting and engaging with different approaches to creating art, for expanding our horizons and creative practices, and most importantly, to develop a better understanding of ourselves and our place in the world.</p>
           </div>
 
           <div id="subscribe-area">
-            <span id="subscribe-title">Subscribe to our newsletter for updates, opportunities, & open calls.</span>
-            <br/>
+            <span id="subscribe-title">Subscribe to our newsletter for updates,<br/> opportunities, & open calls.</span>
             <form id="subscribe-form" action="https://tinyletter.com/propagator-zine" method="post" target="popupwindow" onSubmit={this.subscribeOnSubmit}>
               <input type="text" placeholder="email address" name="email" id="tlemail"></input>
               <input className="form-btn" type="submit" value="submit"></input>
             </form>
-         </div>
-
+          </div>
         </div>
 
         <div className="landing-right">
@@ -40,24 +38,22 @@ class Landing extends Component {
 
           <div id="issue-section">
             <div id="issue-section-details">
-              <span id="issue-number">Issue 01</span>
-              <br/>
+              <h3 id="issue-number">Issue 01</h3>
               <span id="issue-title">Dream Machine</span>
               <div id="line-divider"></div>
-              <span id="issue-contributors">Ann Chen, Lynn Hu, James Proctor, Alex Chan, Michaela Costello, ODBOL, Yousif Al-Yousifi, ILOVECOMPUTERS, & Ashwath ANAND</span>
-            </div>
-            <div id="issue-link">
-              <Link to="/issue-01">
-                  <span id="issue-link">Click to read</span>
-                  <img
-                    id="issue-link-img"
-                    alt="Open Issue 01 Arrow"
-                    src={ require(`../assets/arrow-right.png`) }
-                  />
-              </Link>
+              <span id="issue-contributors">Ann Chen // Lynn Hu // James Proctor // Alex Chan // Michaela Costello // Odbol // Yousif Al-Yousifi // ILOVECOMPUTERS // Ashwath ANAND</span>
             </div>
           </div>
-          
+          <div id="issue-link">
+            <Link to="/issue-01">
+              <span id="issue-read-link">Click to read</span>
+              <img
+                id="issue-link-img"
+                alt="Open Issue 01 Arrow"
+                src={ require(`../assets/arrow-right.png`) }
+              />
+            </Link>
+          </div>
         </div>
       </div>
     );
